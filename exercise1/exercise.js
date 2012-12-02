e21d70455ab7f6c91aa1717c4de753113b773dc0
George Lee <keoki.lee@gmail.com>
2012-12-01 16:44:30 -1000

    Initial commit.

---
+++ exercise1/exercise.js
@@ -0,0 +1,3 @@
+// Part 1: Create a function called "hello". This function should return the string "Hello".
+
+// Part 2: Create a function called "greet". This function should take a name as a parameter and then return "Hello <name>".
