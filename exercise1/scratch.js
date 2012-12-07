09e85d244aea3d0abe1e2cfbf1d29e8dea9af02b
George Lee <keoki.lee@gmail.com>
2012-12-06 22:13:18 -1000

    Implemented exercise 2.

---
+++ exercise1/scratch.js
@@ -0,0 +1,2 @@
+// This is just a place to test out some code.
+// Reload index.html when you make changes to this file.
---
+++ exercise2/exercise.js
@@ -0,0 +1,23 @@
+// Part 1.1: Write a function that takes a name and price and returns an object.
+// The object's properties should be "name" and "price".
+function createItem(name, price) {
+    return {};
+}
+
+// Part 1.2: Create a function that takes an array and an object and adds it to the list.
+// It should return the new list.
+function addOrder(array, item) {
+    return [];
+}
+
+// Part 2.1: Write a function that goes through an array and adds up the orders.
+function total(array) {
+    return 0;
+}
+
+// Part 2.2: Create a function that takes an array of orders and calculates the tip.
+// The tip should be 15% if there are less than 6 items, else it should be 20%.
+// Use this function in your total method above to calculate the actual total.
+function tip(array) {
+    return 0;
+}
