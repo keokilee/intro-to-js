d53075fe355127511968c69721eb61dc99ae2135
George Lee <keoki.lee@gmail.com>
2012-12-07 22:15:10 -1000

    Committing content for class.

---
+++ exercise3/exercise.js
@@ -0,0 +1,14 @@
+// Part 1: Listen for the form submission and display an alert. Use the "alert" function.
+
+// Part 2: In index.html are a few hidden elements. Using selectors and the .show() method, display them when the form is submitted.
+// Remove the alert function above.
+
+// Part 3: Modify the function to replace the contents of the text boxes with an empty string.
+// Use the val function to set the values in those inputs.
+
+// Part 4: Modify the function and append the string "<li>order</li>" to a ul element of class "orders".
+// Try adding effects! (You might want to hide the orders first).
+
+// Part 5: Modify the function and instead insert the contents of the text fields.
+// You will want to use "input[name=name]" and "input[name=price]" and create a new string.
+// Read: http://api.jquery.com/val/
---
+++ exercise5/exercise.js
@@ -0,0 +1,8 @@
+var url = "http://search.twitter.com/search.json?callback=?&count=100&rpp=100&q=honolulu";
+
+// Retrieve data from the above URL using $.getJSON and display it in a div with the class "tweets".
+// You don't need to parse the data you get back, as getJSON will do that for you.
+// You might want to print the data you get back in the console to see what it looks like.
+$(function() {
+
+});
